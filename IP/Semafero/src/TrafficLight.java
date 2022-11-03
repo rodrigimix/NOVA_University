dd98ff2cb41661235bfd0b7f6cb7ea4a9f5e5dfe
Rodrigo <rodrigimix@protonmail.com>
2022-11-03 20:59:18 +0000

    done for now

--- IP/Semafero/src/TrafficLight.java
+++ IP/Semafero/src/TrafficLight.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class TrafficLight {
+public class TrafficLight {
+    private boolean green, yellow, red;
+    public TrafficLight(){
+        green = false;
+        yellow = false;
+        red = true;
+    }
+    public boolean isRed(){
+        return red;
+    }
+    public boolean isGreen(){
+        return green;
+    }
+    public boolean isYellow(){
+        return yellow;
+    }
+    public boolean mustStop(){
+        return red;
+    }
+    public boolean canGo(){
+        if (!red){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void changeColor(){
+        if (green){
+            green = false;
+            yellow = true;
+        }
+        else if(yellow){
+            yellow = false;
+            red = true;
+        }
+        else {
+            red = false;
+            green = true;
+        }
+    }
 }
