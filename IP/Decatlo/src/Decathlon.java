a2cfc379ffb4433025d4342501588f8ceb1fa1f9
Rodrigo <rodrigimix@protonmail.com>
2022-11-04 22:15:50 +0000

    Done for now!

--- IP/Decatlo/src/Decathlon.java
+++ IP/Decatlo/src/Decathlon.java
@@ -11,32 +11,98 @@ public class Decathlon {
     private static final double D_A = 12.91, D_B = 4.00, D_C = 1.10;
     private static final double J_A = 10.14, J_B = 7.00, J_C = 1.08;
 
-    private double points;
+    private double m100, m400, m1500, m110, shot, discuss, javelin;
+    private int HJ, LJ, PVJ;
 
     public Decathlon(){
-        points = 0;
+        m100 = 0;
+        m400 = 0;
+        m1500 = 0;
+        m110 = 0;
+        shot = 0;
+        discuss = 0;
+        javelin = 0;
+        HJ = 0;
+        LJ = 0;
+        PVJ = 0;
     }
 
     public int getPoints(){
-        return Math.toIntExact(Math.round(points));
+        return Math.toIntExact(Math.round(m100+m400+m1500+m110+shot+discuss+javelin+HJ+LJ+PVJ));
     }
 
     public void set100Meters(double t){
         if (t > 0){
-            points += CEM_A * Math.pow((CEM_B - t), CEM_C);
+            m100 = CEM_A * Math.pow((CEM_B - t), CEM_C);
         }
     }
 
     public void set400Meters(double t){
         if (t > 0){
-            points += QUATRO_A * Math.pow((QUATRO_B - t), QUATRO_C);
+            m400 = QUATRO_A * Math.pow((QUATRO_B - t), QUATRO_C);
         }
     }
 
     public void set1500Meters(double t){
         if (t > 0){
-            points += MILQUINHENHROS_A * Math.pow((MILQUINHENHROS_B - t), MILQUINHENHROS_C);
+            m1500 = MILQUINHENHROS_A * Math.pow((MILQUINHENHROS_B - t), MILQUINHENHROS_C);
         }
     }
+
+    public void set110MetersHurdles(double t){
+        if (t > 0){
+            m110 = CEMDEZ_A * Math.pow((CEMDEZ_B - t), CEMDEZ_C);
+        }
+    }
+
+    public void setHighJump(int m){
+        if (m > 0){
+            HJ = Math.toIntExact(Math.round(HJ_A * Math.pow((m - HJ_B), HJ_C)));
+        }
+    }
+
+    public void setLongJump(int m){
+        if (m > 0){
+            LJ = Math.toIntExact(Math.round(LJ_A * Math.pow((m - LJ_B), LJ_C)));
+        }
+    }
+
+    public void setPoleVaultJump(int m){
+        if (m > 0){
+            PVJ = Math.toIntExact(Math.round(PV_A * Math.pow((m - PV_B), PV_C)));
+        }
+    }
+
+    public void setShot(double d){
+        if (d > 0){
+            shot = S_A * Math.pow((d - S_B), S_C);
+        }
+    }
+
+    public void setDiscus(double d){
+        if (d > 0){
+            discuss = D_A * Math.pow((d - D_B), D_C);
+        }
+    }
+
+    public void setJavelin(double d){
+        if (d > 0){
+            javelin = J_A * Math.pow((d - J_B), J_C);
+        }
+    }
+
+    public void reset(){
+        m100 = 0;
+        m400 = 0;
+        m1500 = 0;
+        m110 = 0;
+        shot = 0;
+        discuss = 0;
+        javelin = 0;
+        HJ = 0;
+        LJ = 0;
+        PVJ = 0;
+    }
+
 
 }
--- IP/Decatlo/src/Main.java
+++ IP/Decatlo/src/Main.java
@@ -3,6 +3,19 @@ public class Main {
         Decathlon d = new Decathlon();
         d.set100Meters(10.4);
         System.out.println(d.getPoints());
-        // 999 (int)
+// 999 (int)
+        d.reset();
+        System.out.println(d.getPoints());
+// 0 (int)
+        d.set100Meters(10.4);
+        d.setLongJump(736);
+        System.out.println(d.getPoints());
+// 1900 (int)
+        d.setShot(13.53);
+        System.out.println(d.getPoints());
+// 2600 (int)
+        d.setHighJump(210);
+        System.out.println(d.getPoints());
+// 3497 (int)
     }
 }
